13e50072b1780a8725e2e6c410a0fa59127b4340
vadsirisko <vadim.sirisko@onilab.com>
2023-03-01 22:28:44 +0300

    CW-4: Narcissistic

--- index.js
+++ index.js
@@ -119,3 +119,50 @@ function longestRepetition(s) {
 longestRepetition("aaaabb");      // ["a",4]
 longestRepetition("bbbaaabaaaa");      // ["a",7]
 longestRepetition("abbbbb");      // ["b",5] )
+
+// A Narcissistic Number (or Armstrong Number) is a positive number which is the sum of its own digits, each raised to the power of the number of digits in a given base. In this Kata, we will restrict ourselves to decimal (base 10).
+
+// For example, take 153 (3 digits), which is narcissistic:
+
+// 1^3 + 5^3 + 3^3 = 1 + 125 + 27 = 153
+// and 1652 (4 digits), which isn't:
+//
+// 1^4 + 6^4 + 5^4 + 2^4 = 1 + 1296 + 625 + 16 = 1938
+// The Challenge:
+//
+// Your code must return true or false (not 'true' and 'false') depending upon whether the given number is a Narcissistic number in base 10.
+//
+// This may be True and False in your language, e.g. PHP.
+//
+// Error checking for text strings or other invalid inputs is not required, only valid positive non-zero integers will be passed into the function.
+
+// first variant
+function narcissistic(value) {
+    if (!value) {
+        return false
+    }
+
+    const numberValueToString = `${value}`
+    const numberLength = numberValueToString.length
+
+    const narcissisticNumberSummary = numberValueToString.split('').reduce((accumulator, currentValue) => {
+        return accumulator + Math.pow(currentValue, numberLength)
+    }, 0)
+
+    return value === narcissisticNumberSummary
+}
+
+// second variant
+function narcissisticSecondVariant(value) {
+    const currentValueToString = `${value}`
+
+    return currentValueToString.split('').reduce(function(accumulator, currentValue) {
+        return accumulator + Math.pow(currentValue, currentValueToString.length)
+    }, 0) === value
+}
+
+narcissistic(7); // true
+narcissistic(153); // true
+narcissistic(122); // false
+narcissistic(1652); // false
+narcissisticSecondVariant(1652); // false
