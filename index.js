1f75b6e0e747f8408f20e69d24ad4cd47087ae19
vadsirisko <vadim.sirisko@onilab.com>
2023-05-06 12:02:17 +0300

    CW-8: First non-repeating character

--- index.js
+++ index.js
@@ -284,3 +284,21 @@ function rotateTablet(s){
 }
 
 rotateTablet("Hello") // => ["elloH", "lloHe", "loHel", "oHell", "Hello"]
+
+// Write a function named first_non_repeating_letter that takes a string input, and returns the first character that is not repeated anywhere in the string.
+// For example, if given the input 'stress', the function should return 't', since the letter t only occurs once in the string, and occurs first in the string.
+// As an added challenge, upper- and lowercase letters are considered the same character, but the function should return the correct case for the initial letter. For example, the input 'sTreSS' should return 'T'.
+// If a string contains all repeating characters, it should return an empty string ("") or None -- see sample tests.
+
+function firstNonRepeatingLetter(str) {
+    return str.split('').find((currentLetter) => {
+        if(str.match(new RegExp(`${currentLetter}`, 'gi')).length === 1) {
+            return currentLetter
+        }
+    }) || ''
+}
+
+firstNonRepeatingLetter('a') // 'a'
+firstNonRepeatingLetter('abba') // ''
+firstNonRepeatingLetter('stress')  // 't'
+firstNonRepeatingLetter('moonmen') // 'e'
