7902c283cd8324c523b1cb8727db1f5cf875fe1a
vaq1a <vadimis26@gmail.com>
2024-09-22 18:56:54 +0300

    LC-10: Sleep function

--- index.js
+++ index.js
@@ -328,3 +328,17 @@ function uncommonFromSentences(s1, s2) {
 }
 
 uncommonFromSentences(s1, s2)
+
+
+// Given a positive integer millis, write an asynchronous function that sleeps for millis milliseconds. It can resolve any value.
+
+async function sleep(millis) {
+    await new Promise((resolve) => {
+        setTimeout(() => {
+            resolve()
+        }, millis)
+    })
+}
+
+// let t = Date.now()
+// sleep(100).then(() => console.log(Date.now() - t)) // 100
